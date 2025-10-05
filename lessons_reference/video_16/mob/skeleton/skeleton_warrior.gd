60499840725de2a319768a5144abe3ce5ca4c6ea
Tanawat Tanatka <tanawat.tan@kkumail.com>
2025-10-06 04:02:20 +0700

    Edit mob and spawner
    เปลี่ยนโมเดลมอนและจดเกิด

---
+++ lessons_reference/video_16/mob/skeleton/skeleton_warrior.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+@onready var animation_tree = %AnimationTree
+@onready var animation_player = %AnimationPlayer
+
+func hurt():
+	animation_tree.set("parameters/OneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+func die():
+	animation_player.speed_scale = 1.75
+	animation_player.play("Death_B")
